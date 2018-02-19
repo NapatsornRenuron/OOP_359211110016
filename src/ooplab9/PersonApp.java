d1930865517990f177d1a03c1dd653ba79775cbc
LABCOM_6 <Napatsornprae29@gmail.com>
2018-02-19 13:54:20 +0700

    PersonApp

---
+++ src/ooplab9/PersonApp.java
@@ -0,0 +1,18 @@
+package ooplab9;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        person person = new person("Thadarat","25435954213",
+                new Address("303/63 M.3","SuratThani","84140"),
+                new Job("Staff","20000"));
+
+
+        System.out.println(person.toString());
+
+        person.getJob().setsalary("30000");
+
+        System.out.println(person.getJob());
+
+    }//main
+
+}//class
